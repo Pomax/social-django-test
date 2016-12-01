e62c96f650a62eb810eb1e475406d953bb216278
Pomax <pomax@nihongoresources.com>
2016-12-01 15:03:42 -0800

    post data test

--- server/users/urls.py
+++ server/users/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^logout', views.forceLogout, name='logout'),
     url(r'^oauth2callback', views.callback, name='oauthcallback'),
+    
+    # test routes
+    url(r'^post', views.post_test, name="POST test"),
 ]
--- server/users/views.py
+++ server/users/views.py
@@ -4,8 +4,9 @@ from httplib2 import Http
 from oauth2client import client
 
 from django.core import serializers
-from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.http import (HttpResponse, HttpResponseNotFound)
+from django.template import loader
+from django.shortcuts import (redirect, render)
 from django.contrib.auth import login, logout
 from apiclient.discovery import build
 
@@ -29,11 +30,15 @@ flow = client.flow_from_clientsecrets(
 Initial page with a link that lets us sign in through Google
 """
 def index(request):
-    user = request.user
-    if user.is_authenticated:
-        return HttpResponse("Hello "+user.email+"! You are already logged in, do you want to <a href='/logout'>log out?</a>")
-    auth_uri = flow.step1_get_authorize_url()
-    return HttpResponse("Hello there! <a href='" + str(auth_uri) + "'>Click here to log in</a>")
+    request.session['state'] = GoogleUser.objects.make_random_password()
+    request.session['nonce'] = GoogleUser.objects.make_random_password()
+    flow.params['state'] = request.session['state']
+    
+    return render(request, 'users/index.html', {
+        'user': request.user,
+        'auth_url': flow.step1_get_authorize_url(),
+        'nonce': request.session['nonce']
+    })
 
 
 """
@@ -41,10 +46,8 @@ An explicit logout route.
 """
 def forceLogout(request):
     user=request.user
-    if not user.is_authenticated:
-        return redirect("/")
-    email=user.email
-    logout(request)
+    if user.is_authenticated:
+        logout(request)
     return redirect("/")
 
 
@@ -60,12 +63,19 @@ def callback(request):
         return HttpResponse("login failed: " + str(error))
 
     if auth_code is not False:
+        state = request.GET.get('state',False)
+
+        if state is False:
+            return HttpResponse("Questionable login: missing state value in callback.")
+
+        if state != request.session['state']:
+            return HttpResponse("Questionable login: incorrect state value in callback.")
+
+        # get the authenticating user's name and email address from the Google API
         credentials = flow.step2_exchange(auth_code)
         http_auth = credentials.authorize(Http())
         service = build('oauth2', 'v2', http=http_auth)
         userinfo = service.userinfo().get().execute()
-
-        # get this user's information
         name = userinfo['name']
         email = userinfo['email']
 
@@ -73,8 +83,11 @@ def callback(request):
             # Get the db record for this user and make sure their
             # name matches what google says it should be.
             user = GoogleUser.objects.get(email=email)
+            # Just to be safe, we rebind the user's name, as this may have
+            # changed since last time we saw this user.
             user.name = name
-            print "found user in database based on email address."
+            user.save()
+            print("found user in database based on email address.")
 
         except GoogleUser.DoesNotExist:
             # Create a new database entry for this user.
@@ -82,15 +95,51 @@ def callback(request):
                 name=name,
                 email=email
             )
-            print "user not found: created user based on email address."
+            print("user not found: created user based on email address.")
 
         # As this user just authenticated, we mark this user as logged in
         # for the duration of this session.
         login(request, user)
 
-        request.session['csrf'] = "lolcakes";
+        return redirect('/')
 
-        #return HttpResponse("login succeeded, with code: " + str(auth_code))
-        return HttpResponse("User "+email+" logged in.")
+    return HttpResponseNotFound("callback happened without an error or code query argument: wtf")
 
-    return HttpResponse("callback happened without an error or code query argument: wtf")
+"""
+Testing route for POSTing to the Django database
+"""
+def post_test(request):
+    user = request.user
+    csrf_token = request.POST.get('csrfmiddlewaretoken', False)
+    nonce = request.POST.get('nonce', False)
+    
+    # ignore post attempts without a CSRF token
+    if csrf_token is False:
+        return HttpResponseNotFound("No CSRF token in POST data.")
+    
+    # ignore post attempts without a known form id
+    if nonce is False:
+        return HttpResponseNotFound("No form identifier in POST data.")
+
+    # ignore post attempts by clients that are not logged in 
+    if not user.is_authenticated:
+        return HttpResponseNotFound("Anonymous posting is not supported.")
+
+    # ignore unexpected post attempts (i.e. missing the session-based unique form id)
+    if nonce != request.session['nonce']:
+        # invalidate the nonce entirely, so people can't retry until there's an id collision
+        request.session['nonce'] = False
+        return HttpResponseNotFound("Forms cannot be auto-resubmitted (e.g. by reloading).")
+
+    # invalidate the nonce, so this form cannot be resubmitted with the current id
+    request.session['nonce'] = False
+
+    # We're finally in a position to accept this data.
+
+    context = {
+        'user': user,
+        'name': request.POST['name'],
+        'url': request.POST['url'],
+    }
+
+    return render(request, 'users/submissions.html', context)
